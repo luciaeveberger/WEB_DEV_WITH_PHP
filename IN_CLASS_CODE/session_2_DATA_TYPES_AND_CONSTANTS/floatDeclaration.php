c745662ea722b3b996f32859dd241e43459b0d49
lucia berger <berger.luciaeve@gmail.com>
2021-01-25 10:35:05 -0500

    refactoring of base code
    Examples of this refactoring

--- IN_CLASS_CODE/session_2_DATA_TYPES_AND_CONSTANTS/floatDeclaration.php
+++
@@ -1,6 +0,0 @@
-<?php
-$exampleFloatValue = 10.365;
-var_dump($exampleFloatValue);
-echo "Our float value 'exampleFloatValue';
-echo "Our float value "exampleFloatValue"";
-echo "Our float value '$exampleFloatValue'";

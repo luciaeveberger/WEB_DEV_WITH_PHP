69baff6c21f34fd566581fe035ee4be39294b04c
lucia berger <berger.luciaeve@gmail.com>
2021-03-29 09:44:30 -0400

    adds subfolder

--- IN_CLASS_CODE/SESSION_18/database_example.php
+++ IN_CLASS_CODE/SESSION_18/database_example.php
@@ -8,14 +8,20 @@ $db     = 'php_work';
 
 $conn  = mysqli_connect($dbhost,$dbuser,'',$db);
 
+# what are we doing in this satement -> we are checking our connection
 if(! $conn){
-    die('Could not connect connect: ') ;
+    die('Could not connect: ') ;
 }
 
 echo 'Successfully Connected';
 
+
+$new_test_database = "silly_new_database";
 // USE A KNOWN DATABASE
-$sql = "USE ". $db;
+$sql = "USE ". $db; // + .
+
+$create_db = "CREATE ". $new_test_database;
+
 if ($conn->query($sql) === TRUE) {
     echo "Database created successfully";
 } else {
@@ -23,19 +29,29 @@ if ($conn->query($sql) === TRUE) {
 }
 
 // QUERY WE HAVE
-$sql_query = "SELECT * FROM pet";
+$sql_query = "CREATE TABLE IF NOT EXISTS  (
+    id INT(6) NON_NULLABLE UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+firstname VARCHAR(30) NOT NULL,
+lastname VARCHAR(30) NOT NULL
+)";
 $result = $conn->query($sql_query);
 print_r($result);
 
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo " - Name: " . $row["name"]. "<br>";
-    }
+# remembering -> the size of database does affect this !
+# so if we are dealing with a HUGE databse -> we probably want to paginate
+print_r(mysqli_fetch_all($result)); // in a body that is kind of ugly !
+echo "<\br>";
+# PARSING OUR RESULTS TO SOMETHING LESS UGLY!
+//if ($result->num_rows > 0) {
+//    // output data of each row
+//    while($row = $result->fetch_assoc()) {
+//        echo " - Name: " . $row["name"] . "";
+//        echo " - has a : " . $row["species"]. "";
+//    }
 } else {
     echo "0 results";
 }
-
+# you want to remember when you are done with the database -> THaT YOU CLOSE IT!!
 mysqli_close($conn);
 
 ?>
